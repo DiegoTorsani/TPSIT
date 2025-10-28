bc9d713478abe0369bf2cf352c71083b22a43980
Diego <diego.torsani@studenti.isissgobetti.it>
2025-10-28 15:33:18 +0100

    That's it 2

--- Torsani_Veicolo.py
+++ Torsani_Veicolo.py
@@ -6,12 +6,3 @@ class Veicolo:
 
     def descrizione(self):
         return f"{self.anno} {self.marca} {self.modello}"
-
-class Moto(Veicolo):
-    def __init__(self, marca, modello, anno, tipo):
-        super().__init__(marca, modello, anno)
-        self.tipo = tipo
-
-    def descrizione(self):
-        base_descrizione = super().descrizione()
-        return f"{base_descrizione}, Tipo: {self.tipo}"
