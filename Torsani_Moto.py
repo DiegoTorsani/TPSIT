27acf5e0247ac20714f26acbed491a3f3cd84b18
Diego Torsani <diego.torsani@studenti.isissgobetti.it>
2026-01-27 12:08:52 +0100

    Cancella

--- Torsani_Moto.py
+++
@@ -1,8 +0,0 @@
-class Moto(Veicolo):
-    def __init__(self, marca, modello, anno, tipo):
-        super().__init__(marca, modello, anno)
-        self.tipo = tipo
-
-    def descrizione(self):
-        base_descrizione = super().descrizione()
-        return f"{base_descrizione}, Tipo: {self.tipo}"
--- Torsani_Veicolo.py
+++
@@ -1,8 +0,0 @@
-class Veicolo:
-    def __init__(self, marca, modello, anno):
-        self.marca = marca
-        self.modello = modello
-        self.anno = anno
-
-    def descrizione(self):
-        return f"{self.anno} {self.marca} {self.modello}"
